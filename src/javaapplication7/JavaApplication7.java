e78e2639726bdb271141c76dbbbeb916d8fdd302
zbossik <zbossik@gmail.com>
2015-03-20 19:52:34 +0400

    bbbb

---
+++ src/javaapplication7/JavaApplication7.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Administrator
+ */
+public class JavaApplication7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
