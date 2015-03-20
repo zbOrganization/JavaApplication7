7566ea6e95a25196eeefa40fc4e5da471df2b706
zbossik <zbossik@gmail.com>
2015-03-20 19:56:13 +0400

    ghgfhfgh

--- src/javaapplication7/JavaApplication7.java
+++ src/javaapplication7/JavaApplication7.java
@@ -15,5 +15,6 @@ public class JavaApplication7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello Team!...");
     }
 }
